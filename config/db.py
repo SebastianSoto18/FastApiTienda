c80d336703e9add559f97b68a33ac0a4f296548b
Sebastian18 <atlasjuanse12@gmail.com>
2022-09-21 02:34:26 -0500

    descomentando base de datos

--- config/db.py
+++ config/db.py
@@ -2,12 +2,15 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL ="mysql+pymysql://sql10521207:gfpKq6LLHi@sql10.freemysqlhosting.net:3306/sql10521207"
-#engine=create_engine(SQLALCHEMY_DATABASE_URL)
-#meta=MetaData()
-#conn = engine.connect()
-
-SQLALCHEMY_DATABASE_URL ="mysql+pymysql://root:78945612310@localhost:3306/tienda"
+#base de datos produccion
+SQLALCHEMY_DATABASE_URL ="mysql+pymysql://sql10521207:gfpKq6LLHi@sql10.freemysqlhosting.net:3306/sql10521207"
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 meta=MetaData()
 conn = engine.connect()
+
+
+#base de datos desarrollo
+#SQLALCHEMY_DATABASE_URL ="mysql+pymysql://root:78945612310@localhost:3306/tienda"
+#engine=create_engine(SQLALCHEMY_DATABASE_URL)
+#meta=MetaData()
+#conn = engine.connect()
