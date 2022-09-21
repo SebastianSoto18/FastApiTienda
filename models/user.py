0a8047e8868d6df616521ae907ecd5ccdf357968
Sebastian18 <atlasjuanse12@gmail.com>
2022-09-20 22:01:49 -0500

    añadiendo metodo crear usuario y cambiando bd

--- models/user.py
+++ models/user.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Table, Column
-from sqlalchemy.sql.sqltypes import Integer, String, DateTime
+from sqlalchemy.sql.sqltypes import Integer, String, Text
 from config.db import meta, engine
 
 users = Table("users", meta, 
         Column("id", Integer, primary_key=True), 
         Column("name", String(100)), 
-        Column("email", String(150)), 
-        Column("password", String(50)), 
-        Column("phone", String(11)))
+        Column("email", String(150), unique=True), 
+        Column("password", String(255)), 
+        Column("phone", String(11), unique=True))
 
 meta.create_all(engine)
 
--- routes/user.py
+++ routes/user.py
@@ -1,9 +1,21 @@
 from fastapi import APIRouter
 from config.db import conn
 from models.user import users
+from schemas.user import User
+from cryptography.fernet import Fernet
 
 user = APIRouter()
+
+key=Fernet.generate_key()
+serializer=Fernet(key)
 
 @user.get("/users")
 def get_users():
     return conn.execute(users.select()).fetchall()
+
+@user.post("/users")
+def create_user(user: User):
+    new_user = {"name": user.name, "email": user.email, "phone": user.phone, "password": ""}
+    new_user["password"] = serializer.encrypt(user.password.encode("utf-8"))
+    conn.execute(users.insert().values(new_user))
+    return "creado"
